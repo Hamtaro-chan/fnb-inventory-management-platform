bc048008e97e46fa95e08d56263d9353bbaa8444
Hamtaro-chan <sirimintra.vio@student.mahidol.edu>
2025-12-02 19:07:50 +0700

    Create func_create_supplier_order.sql

---
+++ Fnb inventory management project/func_create_supplier_order.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION create_purchase_order(shopID BIGINT, supplierID BIGINT, totalCost NUMERIC)
+RETURNS BIGINT
+LANGUAGE plpgsql
+AS $$
+DECLARE 
+	new_po_id BIGINT;
+	commission_rate NUMERIC := 0.15; -- it's where the app make money
+	calculated_fee NUMERIC;
+BEGIN
+	calculated_fee := totalCost * commission_rate;
+	INSERT INTO purchase_orders (
+        shop_id, 
+        supplier_id, 
+        total_amount, 
+        commission_fee, 
+        status
+    )
+	VALUES (
+		shopID,
+		supplier_id,
+		totalCost,
+		commission_fee,
+		'ordered'
+	)
+	RETURNING po_id INTO new_po_id;
+END;			
+$$
