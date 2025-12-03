a27a23fa990486c108d5f8ec07334375617b700d
Hamtaro-chan <sirimintra.vio@student.mahidol.edu>
2025-12-03 20:23:44 +0700

    Update add_supplier_data.sql

--- Fnb inventory management project/add_supplier_data.sql
+++ Fnb inventory management project/add_supplier_data.sql
@@ -1,3 +1,7 @@
-INSERT INTO ingredient_supplier (supplier_name, category) VALUES
-('Global Matcha Co', 'Tea'),
-('Dairy Farm', 'Dairy');
+INSERT INTO supplier_goods (supplier_id, ingredient_name, current_cost) VALUES
+(1, 'Premium Matcha 1kg', 500.00),
+(1, 'Nishio Matcha 1kg', 1000.00),
+(1, 'Uji Matcha 1kg', 700.00),
+(1, 'Samidori Matcha 100g', 1300.00),
+(2, 'Lactose-free milk 2L', 150.00),
+(2, 'Fresh Whole Milk 2L', 90.00);
