e96cf6ea786a8a3e4fc69033ac4e2fb715849d66
Hamtaro-chan <sirimintra.vio@student.mahidol.edu>
2025-12-03 18:14:33 +0700

    Create add_shop_user_data.sql

---
+++ Fnb inventory management project/add_shop_user_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO shop_user (shop_name, category) VALUES 
+('The Tasty Table', 'Restaurant'),
+('Yummy Yum', 'Restaurant'),
+('Seasonal & Co', 'Coffee Shop'),
+('Eatopia', 'Bakery'),
+('Hamtaro Coffee', 'Coffee Shop');
