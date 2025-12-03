b78767238aa1a8ed391909283c1d71135dba0a80
Hamtaro-chan <sirimintra.vio@student.mahidol.edu>
2025-12-03 19:43:37 +0700

    add menu ingredient data

---
+++ Fnb inventory management project/add_menu_ingredients_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO menu_ingredients (menu_id, item_id, required_unit) VALUES
+--Matcha Latte
+(1, 1, 5),
+(1, 2, 150),
+(1, 9, 20),
+
+--Minty Choco
+(2, 4, 30),
+(2, 2, 200),
+(2, 9, 20),
+
+--Americano
+(3, 7, 18);
