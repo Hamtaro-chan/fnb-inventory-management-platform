8875e81f973b6b554f4b23de7358c8ff2e84301e
Hamtaro-chan <sirimintra.vio@student.mahidol.edu>
2025-12-04 04:44:18 +0700

    Update tableeeee.sql

--- Fnb inventory management project/tableeeee.sql
+++ Fnb inventory management project/tableeeee.sql
@@ -1,9 +1,8 @@
-INSERT INTO menu_ingredients (menu_id, item_id, required_unit) VALUES
-((SELECT menu_id FROM shop_menu WHERE menu_name = 'Butter Croissant'), (SELECT item_id FROM shop_item_info WHERE item_name = 'All Purpose Flour' AND shop_id = 2), 1),
-((SELECT menu_id FROM shop_menu WHERE menu_name = 'Butter Croissant'), (SELECT item_id FROM shop_item_info WHERE item_name = 'Unsalted Butter' AND shop_id = 2), 1);
-
--- Recipe: Chocolate Cake (Flour + Chocolate + Eggs)
-INSERT INTO menu_ingredients (menu_id, item_id, required_unit) VALUES
-((SELECT menu_id FROM shop_menu WHERE menu_name = 'Chocolate Lava Cake'), (SELECT item_id FROM shop_item_info WHERE item_name = 'All Purpose Flour' AND shop_id = 2), 1),
-((SELECT menu_id FROM shop_menu WHERE menu_name = 'Chocolate Lava Cake'), (SELECT item_id FROM shop_item_info WHERE item_name = 'Dark Chocolate Chips' AND shop_id = 2), 1),
-((SELECT menu_id FROM shop_menu WHERE menu_name = 'Chocolate Lava Cake'), (SELECT item_id FROM shop_item_info WHERE item_name = 'Fresh Eggs' AND shop_id = 2), 2);
+INSERT INTO ingredient_supplier (supplier_name, category) VALUES
+('Bakery Best Ltd', 'Baking Supplies'),
+('Golden Grain Flour', 'Dry Goods'),
+('Happy Hen Farms', 'Dairy & Eggs'),
+('Imported Chocolates Co', 'Sweets'),
+('Pack-It Packaging', 'Packaging'),
+('Spice World', 'Spices'),
+('Local Dairy Co-op', 'Dairy');
