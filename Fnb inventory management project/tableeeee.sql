9c13f070fda68321d759e3acfefc3bff319cb267
Hamtaro-chan <sirimintra.vio@student.mahidol.edu>
2025-12-04 04:45:51 +0700

    Update tableeeee.sql

--- Fnb inventory management project/tableeeee.sql
+++ Fnb inventory management project/tableeeee.sql
@@ -1,11 +1,5 @@
-INSERT INTO customer_orders (shop_id, total_price, status) VALUES
-(2, 65.00, 'paid'),
-(2, 130.00, 'paid'),
-(2, 55.00, 'paid'),
-(2, 120.00, 'paid'),
-(2, 200.00, 'paid'),
-(2, 65.00, 'paid'),
-(2, 500.00, 'paid'),
-(2, 80.00, 'paid'),
-(2, 150.00, 'cancelled'),
-(2, 110.00, 'paid');
+INSERT INTO customer_order_items (order_id, menu_id, quantity, price_at_sale) VALUES
+((SELECT MAX(order_id) FROM customer_orders), (SELECT menu_id FROM shop_menu WHERE menu_name = 'Butter Croissant'), 2, 65.00),
+((SELECT MAX(order_id) - 1 FROM customer_orders), (SELECT menu_id FROM shop_menu WHERE menu_name = 'Chocolate Lava Cake'), 1, 120.00),
+((SELECT MAX(order_id) - 2 FROM customer_orders), (SELECT menu_id FROM shop_menu WHERE menu_name = 'Vanilla Cupcake'), 4, 55.00),
+((SELECT MAX(order_id) - 3 FROM customer_orders), (SELECT menu_id FROM shop_menu WHERE menu_name = 'Sourdough Loaf'), 1, 150.00);
